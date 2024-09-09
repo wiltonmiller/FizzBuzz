d813a0c02b121907fca52901f569f47e02927b61
wilonmiller <150298993+wiltonmiller@users.noreply.github.com>
2024-09-09 10:10:42 -0400

    Made changes to the Multiples file

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,15 +1,4 @@
-public class Multiples {
+package PACKAGE_NAME;
 
-    public static void main(String args) {
-        System.out.println(countMultiples(1000));
-    }
-    public static int countMultiples(int number) {
-        int multiples = 0;
-        for (int i = 0; i <= number; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                multiples++;
-            }
-        }
-        return multiples;
-    }
+public class Multiples {
 }
